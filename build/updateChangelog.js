2535abe77947a6a89836560b249b15051cdfd2e6
dera- <alchemy.and.fairy@gmail.com>
2019-04-04 19:13:21 +0900

    fix pkg001

--- build/updateChangelog.js
+++ build/updateChangelog.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-const semver = require("semver");
 const execSync = require("child_process").execSync;
 
 if (process.argv.length < 3) {
@@ -15,25 +14,30 @@ if (! /^patch|minor|major$/.test(target)) {
 	process.exit(1);
 }
 
+const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
 // 更新するモジュールが無ければChangelog更新処理を行わず終了する
-if (parseInt(execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna")} changed | wc -l`).toString(), 10) === 0) {
+if (parseInt(execSync(`${lernaPath} changed | wc -l`).toString(), 10) === 0) {
 	console.error("No modules to update version.");
 	process.exit(1);
 }
 
-// lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要があるが、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする
+// lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要がある。
+// しかし、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする。
 if (process.env.GITHUB_AUTH == null) {
 	console.error("Must provide GITHUB_AUTH.");
 	process.exit(1);
 }
 
-// 全akashic-cli-xxxに依存するakashic-cliモジュールの次のバージョン番号を取得
-const packageJson = require(path.join(__dirname, "..", "packages", "pkg002", "package.json"));
-const nextVersion = semver.inc(packageJson["version"], target);
+// publish処理
+const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version; // CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
+execSync(`${lernaPath} publish pitch --yes`);
 
 // 現在のCHANGELOGに次バージョンのログを追加
+console.log("add changelog");
+const currentVersion = require(path.join(__dirname, "..", "lerna.json")).version;
 const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
-const addedLog = 
-	execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog")} --next-version ${nextVersion}`).toString();
+const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
+const addedLog = execSync(`${lernaChangeLogPath} --from ${beforeVersion} --next-version ${currentVersion}`).toString();
 const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
 fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
+execSync("git add ./CHANGELOG.md && git commit -m 'Update Changelog'");
