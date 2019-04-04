57eabd7715b8b33ed312efb8e98c013c7f7a4f2c
dera- <alchemy.and.fairy@gmail.com>
2019-04-04 17:24:53 +0900

    change to fix mode

--- build/updateChangelog.js
+++ build/updateChangelog.js
@@ -8,17 +8,32 @@ if (process.argv.length < 3) {
 	process.exit(1);
 }
 
-// 対象のバージョンナンバー名を取得
+// どのバージョンを上げるのかを取得
 var target = process.argv[2];
 if (! /^patch|minor|major$/.test(target)) {
-	console.error("please specify patch or minor or major");
+	console.error("Please specify patch, minor or major.");
 	process.exit(1);
 }
 
-const modules = JSON.parse(execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna")} ls --toposort --json`).toString().replace("\n", ""));
-const currentVersion = modules[modules.length - 1]["version"];
-const nextVersion = semver.inc(currentVersion, target);
-const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md"));
-const addedLog = execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog")}`).replace("Unreleased", nextVersion);
-const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n\n" + addedLog);
-fs.writeSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
+// 更新するモジュールが無ければChangelog更新処理を行わず終了する
+if (parseInt(execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna")} changed | wc -l`).toString(), 10) === 0) {
+	console.error("No modules to update version.");
+	process.exit(1);
+}
+
+// lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要があるが、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする
+if (process.env.GITHUB_AUTH == null) {
+	console.error("Must provide GITHUB_AUTH.");
+	process.exit(1);
+}
+
+// 全akashic-cli-xxxに依存するakashic-cliモジュールの次のバージョン番号を取得
+const packageJson = require(path.join(__dirname, "..", "packages", "pkg002", "package.json"));
+const nextVersion = semver.inc(packageJson["version"], target);
+
+// 現在のCHANGELOGに次バージョンのログを追加
+const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
+const addedLog = 
+	execSync(`${path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog")} --next-version ${nextVersion}`).toString();
+const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
+fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
