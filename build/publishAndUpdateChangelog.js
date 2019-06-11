b2543265ab17efbfd682cc338b97ce0a0d6dd2f5
dera- <alchemy.and.fairy@gmail.com>
2019-06-11 16:43:58 +0900

    fix build/publishAndUpdateChangelog.js

--- build/publishAndUpdateChangelog.js
+++ build/publishAndUpdateChangelog.js
@@ -43,7 +43,8 @@ console.log("end to publish");
 console.log("start to update changelog");
 let addedLog;
 if (target === "empty") {
-	addedLog = `\n## ${nextVersion}\n* Ignorable change to fix broken publish ${packageJson["version"]}\n`;
+	const currentVersion = require(path.join(__dirname, "..", "lerna.json")).version;
+	addedLog = `\n## ${currentVersion}\n* Ignorable change to fix broken publish ${beforeVersion}\n`;
 } else {
 	const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
 	addedLog = execSync(`${lernaChangeLogPath} --from v${beforeVersion}`).toString();
