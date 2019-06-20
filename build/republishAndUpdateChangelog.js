be1d4a6b284cf8e1e3e85b2a3c3d211dd03d8778
dera- <alchemy.and.fairy@gmail.com>
2019-06-20 16:33:34 +0900

    fix republish-script

--- build/republishAndUpdateChangelog.js
+++ build/republishAndUpdateChangelog.js
@@ -1,23 +1,10 @@
 const path = require("path");
 const fs = require("fs");
 const execSync = require("child_process").execSync;
-const spawnSync = require("child_process").spawnSync;
 
 const apiBaseUrl = "https://api.github.com/repos/dera-/lerna_test";
 const pullRequestBody = "※自動作成されたPRです";
 const pullRequestLabel = "republish";
-
-// function execCommand(command) {
-// 	const words = command.split(" ");
-// 	const result = spawnSync(words[0], words.slice(1));
-// 	if (result.status !== 0) {
-// 		console.log(result.stdout.toString());
-// 		console.log(result.stderr.toString());
-// 		console.error(`Failed: ${command}.`);
-// 		process.exit(1);
-// 	}
-// 	return result.stdout.toString();
-// }
 
 if (process.argv.length < 3) {
 	console.error("Please enter command as follows: node republishAndUpdateChangelog.js [patch|minor|major]");
@@ -38,46 +25,54 @@ if (process.env.GITHUB_AUTH == null) {
 	process.exit(1);
 }
 
-// versionのbump処理
-console.log("start to bump version");
-const branchName = "tmp_" + Date.now();
-const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
-// versionのbumpを行う前の準備作業
-execSync("git fetch");
-execSync("git checkout origin/master");
-execSync(`git checkout -b ${branchName}`);
-execSync("git commit --allow-empty -m 'empty'");
-execSync(`git push origin ${branchName}`);
-// versionのbumpしてcommit+push(ここでgithubリポジトリにタグとリリースノートが作成される)
-execSync(`${lernaPath} version ${target} --allow-branch=${branchName} --force-publish=* --yes`);
-console.log("end to bump version");
+try {
+	// versionのbump処理
+	console.log("start to bump version");
+	const branchName = "tmp_" + Date.now();
+	const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
+	// versionのbumpを行う前の準備作業
+	execSync("git fetch");
+	execSync("git checkout origin/master");
+	execSync(`git checkout -b ${branchName}`);
+	// 直前のcommitがlernaでversionをbumpしたcommitの場合、versionのbumpに失敗するので空コミットを一度挟んでおく
+	execSync("git commit --allow-empty -m 'empty'");
+	// versionをbumpするためにはリモート側にブランチを用意しておく必要がある
+	execSync(`git push origin ${branchName}`);
+	// versionのbumpしてcommit+push(ここでgithubリポジトリにタグとリリースノートが作成される)
+	execSync(`${lernaPath} version ${target} --allow-branch=${branchName} --force-publish=* --yes`);
+	console.log("end to bump version");
 
-// PRの作成とマージ処理
-console.log("start to create PR");
-const currentVersion = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "lerna.json")).toString()).version;
-// PRを作成する
-const pullReqDataString = execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X POST -d '{"title":"v${currentVersion}", "body":"${pullRequestBody}", "head":"dera-:${branchName}", "base":"master"}' ${apiBaseUrl}/pulls`).toString();
-const pullReqData = JSON.parse(pullReqDataString);
-// issue(PR)にラベル付ける
-execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X POST -d '{"labels": ["${pullRequestLabel}"]}' ${apiBaseUrl}/issues/${pullReqData["number"]}/labels`);
-// PRのマージ
-execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X PUT ${apiBaseUrl}/pulls/${pullReqData["number"]}/merge`);
-// ブランチ削除
-execSync("git checkout origin/master");
-execSync(`git branch -D ${branchName}`);
-execSync(`git push origin :${branchName}`);
-console.log("end to merge PR");
+	// PRの作成とマージ処理
+	console.log("start to create PR");
+	// requireの場合bump前のバージョンを取得してしまうため、fs.readFileSyncを使用してbump後のバージョンを取得する
+	const currentVersion = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "lerna.json")).toString()).version;
+	// PRを作成する
+	const pullReqDataString = execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X POST -d '{"title":"v${currentVersion}", "body":"${pullRequestBody}", "head":"dera-:${branchName}", "base":"master"}' ${apiBaseUrl}/pulls`).toString();
+	const pullReqData = JSON.parse(pullReqDataString);
+	// issue(PR)にラベル付ける
+	execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X POST -d '{"labels": ["${pullRequestLabel}"]}' ${apiBaseUrl}/issues/${pullReqData["number"]}/labels`);
+	// PRのマージ
+	execSync(`curl --fail -H "Authorization: token ${process.env.GITHUB_AUTH}" -X PUT ${apiBaseUrl}/pulls/${pullReqData["number"]}/merge`);
+	// ブランチ削除
+	execSync("git checkout origin/master");
+	execSync(`git branch -D ${branchName}`);
+	execSync(`git push origin :${branchName}`);
+	console.log("end to merge PR");
 
-// 現在のCHANGELOGに次バージョンのログを追加
-console.log("start to update changelog");
-execSync("git checkout master");
-execSync("git pull origin master");
-const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
-const addedLog = execSync(`${lernaChangeLogPath} --next-version v${currentVersion}`).toString();
-const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
-const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
-fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
-execSync("git add ./CHANGELOG.md");
-execSync("git commit -m 'Update Changelog'");
-execSync("git push origin master");
-console.log("end to update changelog");
+	// 現在のCHANGELOGに次バージョンのログを追加
+	console.log("start to update changelog");
+	execSync("git checkout master");
+	execSync("git pull origin master");
+	const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
+	const addedLog = execSync(`${lernaChangeLogPath} --next-version v${currentVersion}`).toString();
+	const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
+	const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
+	fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
+	execSync("git add ./CHANGELOG.md");
+	execSync("git commit -m 'Update Changelog'");
+	execSync("git push origin master");
+	console.log("end to update changelog");
+} catch (e) {
+	console.error(e);
+	process.exit(1);
+}
