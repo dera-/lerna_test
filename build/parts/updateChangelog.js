522dca8ab034c0000a22c2a2dfd6e121c188d95c
dera- <alchemy.and.fairy@gmail.com>
2019-06-24 18:24:01 +0900

    fix script

--- build/parts/updateChangelog.js
+++ build/parts/updateChangelog.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const execSync = require("child_process").execSync;
 
-exports = fromVersion => {
+exports = function (fromVersion) {
 	const lernaChangeLogPath = path.join(__dirname, "..", "..", "node_modules", ".bin", "lerna-changelog");
 	const addedLog = execSync(`${lernaChangeLogPath} --from v${fromVersion}`).toString();
 	const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "..", "CHANGELOG.md")).toString();
