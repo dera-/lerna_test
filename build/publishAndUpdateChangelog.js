7a4691868401825e2f523b73cfb27926c5f9a032
dera- <alchemy.and.fairy@gmail.com>
2019-06-24 22:58:19 +0900

    update scripts

--- build/publishAndUpdateChangelog.js
+++ build/publishAndUpdateChangelog.js
@@ -14,6 +14,13 @@ if (! /^patch|minor|major$/.test(target)) {
 	process.exit(1);
 }
 
+const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
+// 更新するモジュールが無ければChangelog更新処理を行わず終了する
+if (!process.env.FORCE_PUBLISH && parseInt(execSync(`${lernaPath} changed | wc -l`).toString(), 10) === 0) {
+	console.error("No modules to update version.");
+	process.exit(1);
+}
+
 // lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要がある。
 // しかし、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする。
 if (process.env.GITHUB_AUTH == null) {
@@ -26,8 +33,8 @@ try {
 	console.log("start to publish");
 	// CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
 	const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version;
-	const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
-	execSync(`${lernaPath} publish ${target} --yes`);
+	const forcePublishOption = process.env.FORCE_PUBLISH ? "--force-publish=*" : "";
+	execSync(`${lernaPath} publish ${target} ${forcePublishOption} --yes`);
 	console.log("end to publish");
 
 	// 現在のCHANGELOGに次バージョンのログを追加
