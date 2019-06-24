83aca31a55aa2d68a5d78949531f69317d08bd58
dera- <alchemy.and.fairy@gmail.com>
2019-06-24 22:36:00 +0900

    update scripts

--- build/publishAndUpdateChangelog.js
+++ build/publishAndUpdateChangelog.js
@@ -14,13 +14,6 @@ if (! /^patch|minor|major$/.test(target)) {
 	process.exit(1);
 }
 
-const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
-// 更新するモジュールが無ければChangelog更新処理を行わず終了する
-if (parseInt(execSync(`${lernaPath} changed | wc -l`).toString(), 10) === 0) {
-	console.error("No modules to update version.");
-	process.exit(1);
-}
-
 // lerna-changelogコマンドを実行するために環境変数GITHUB_AUTHにgithubへのアクセストークンを与える必要がある。
 // しかし、与えられていなくてもコマンド実行時にエラーは発生しないのでここで事前にチェックする。
 if (process.env.GITHUB_AUTH == null) {
@@ -28,25 +21,29 @@ if (process.env.GITHUB_AUTH == null) {
 	process.exit(1);
 }
 
-// publish処理
-console.log("start to publish");
-// CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
-const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version;
-console.log("beforeVersion", beforeVersion);
-execSync(`${lernaPath} publish ${target} --yes`);
-console.log("end to publish");
+try {
+	// publish処理
+	console.log("start to publish");
+	// CHANGELOG作成時に必要になるのでpublish前のバージョンを保持しておく
+	const beforeVersion = require(path.join(__dirname, "..", "lerna.json")).version;
+	const lernaPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna");
+	execSync(`${lernaPath} publish ${target} --yes`);
+	console.log("end to publish");
 
-// 現在のCHANGELOGに次バージョンのログを追加
-console.log("start to update changelog");
-const lernaChangeLogPath = path.join(__dirname, "..", "node_modules", ".bin", "lerna-changelog");
-const currentVersion = require(path.join(__dirname, "..", "lerna.json")).version;
-console.log(`${lernaChangeLogPath} --next-version v${currentVersion}`);
-const addedLog = execSync(`${lernaChangeLogPath} --next-version v${currentVersion}`).toString();
-console.log(addedLog);
-const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "CHANGELOG.md")).toString();
-const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
-fs.writeFileSync(path.join(__dirname, "..", "CHANGELOG.md"), nextChangeLog);
-execSync("git add ./CHANGELOG.md");
-execSync("git commit -m 'Update Changelog'");
-execSync("git push origin master");
-console.log("end to update changelog");
+	// 現在のCHANGELOGに次バージョンのログを追加
+	// 新しくタグを打ってからでないと前回のタグからの更新内容が取得できないため、CHANGELOGへの書き込みはpublish後に行う
+	console.log("start to update changelog");
+	const lernaChangeLogPath = path.join(__dirname, "..", "..", "node_modules", ".bin", "lerna-changelog");
+	const addedLog = execSync(`${lernaChangeLogPath} --from v${beforeVersion}`).toString();
+	const currentChangeLog = fs.readFileSync(path.join(__dirname, "..", "..", "CHANGELOG.md")).toString();
+	const nextChangeLog = currentChangeLog.replace("# CHANGELOG\n\n", "# CHANGELOG\n" + addedLog + "\n");
+	fs.writeFileSync(path.join(__dirname, "..", "..", "CHANGELOG.md"), nextChangeLog);
+	execSync("git add ./CHANGELOG.md");
+	execSync("git commit -m 'Update Changelog'");
+	execSync("git push origin master");
+	console.log("end to update changelog");
+} catch (e) {
+	console.error(e);
+	process.exit(1);
+}
+
