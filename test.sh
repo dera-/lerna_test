a5f6561939c192b9b9913d9d74eb905063e9fd72
dera- <alchemy.and.fairy@gmail.com>
2021-01-18 14:44:05 +0000

    update pkg001

--- test.sh
+++
@@ -1,7 +0,0 @@
-AKASHIC_LIB_NAME="@akashic/palyer-info"
-
-            if [[ ${AKASHIC_LIB_NAME} =~ @akashic-extension\/.+ ]]; then
-              echo "A"
-            else
-              echo "B"
-            fi
