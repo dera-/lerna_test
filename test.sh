9545a80fd5bdcbf46c1327a5abe73ed42ca04433
dera- <alchemy.and.fairy@gmail.com>
2021-01-18 14:38:51 +0000

    fix CHANGELOG.md

---
+++ test.sh
@@ -0,0 +1,7 @@
+AKASHIC_LIB_NAME="@akashic/palyer-info"
+
+            if [[ ${AKASHIC_LIB_NAME} =~ @akashic-extension\/.+ ]]; then
+              echo "A"
+            else
+              echo "B"
+            fi
